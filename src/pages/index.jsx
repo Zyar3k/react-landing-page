86a9d2316c3878c2d221048233f83682ffc322af
Arkadiusz Zygan <a.zygan@gmail.com>
2022-05-21 21:33:33 +0200

    Show first section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import HeroSection from "../components/HeroSection";
 import InfoSection from "../components/InfoSection";
+import { homeObjOne } from "../components/InfoSection/Data";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 
@@ -15,7 +16,7 @@ const Home = () => {
       <Sidebar toggle={toggle} isOpen={isOpen} />
       <Navbar toggle={toggle} />
       <HeroSection />
-      <InfoSection />
+      <InfoSection {...homeObjOne} />
     </>
   );
 };
